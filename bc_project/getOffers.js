ba619c29b44b180877d9064987105f9bcd669d92
Labusova <alexandra.labusova@yrbrands.com>
2019-05-09 14:06:59 +0200

    vue is console logging data from offers page woho

--- bc_project/getOffers.js
+++ bc_project/getOffers.js
@@ -1,29 +1,49 @@
 <% response.setContentType("text/html;charset=utf-8"); %>
-
-<%@ page import="xtk:server/jsspcontext.js" %>
-
-
+<%@ page import="xtk:server/jsspcontext.js"%>
 <%@ page import="xtk:shared/nl.js"%>
 <%@ page import="xtk:shared/xtk.js" %>
-
 <%
-
     var context = logonEscalation("admin");
-
-    //get the filtering parameters from url
-
+    
     var chosenMarket = request.getParameter("market");
     var chosenCategory = request.getParameter("category");
-    var chosenStatus = request.getParameter("status");
+    var chosenOfferId = request.getParameter("offerId");
 
-    //hard coded id
-    //var results = "209,846,822";
-    var results = request.getParameter("id");
+    var results = [];
+
+    var query = xtk.queryDef.create(
+        <queryDef schema="nms:offer" operation="select">
+          <select>
+            <node expr="[@label]" alias="@label" />
+            <node expr="[@status]" alias="@status"/>
+            <node expr="[@created]" sort="true" />
+          </select>
+            <orderBy displayOrder="Creation date (descending)">
+             <node expr="[@created]" label="Creation date" sort="1" sortDesc="1"/>
+            </orderBy>
+           <groupBy/>
+           <having/>
+          <where>
+          <condition expr={"[category/@label]= 'Recipe Recommendation' AND [localOrgUnit/@label]= 'Germany'"}/>
+          </where>
+         </queryDef>
+       )
+
+var  selectedOffers = query.ExecuteQuery();
+
+for each (var offer in selectedOffers){
+
+    var block = {
+        "offerId" :offer.@id.toString(),
+        "labelonoffer" :offer.@label.toString(),
+        "category" :offer.category.@label.toString()
+          }
+          results.push(block);
+}
 
 
 
-    //create the json result
+
     document.write(JSON.stringify(results));
-
-//the ending tag of the logon - server side javascript
+    console.log(JSON.stringify(results))
 %><% logonWithContext(context);%>
