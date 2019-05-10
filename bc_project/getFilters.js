de00fd50fda54350372b124d5111935043185e9c
Labusova <alexandra.labusova@yrbrands.com>
2019-05-10 16:40:03 +0200

    changes

---
+++ bc_project/getFilters.js
@@ -0,0 +1,88 @@
+<% response.setContentType("text/html;charset=utf-8"); %>
+<%@ page import="xtk:server/jsspcontext.js"%>
+<%@ page import="xtk:shared/nl.js"%>
+<%@ page import="xtk:shared/xtk.js" %>
+<%
+    var context = logonEscalation("admin");
+
+    var results = {"MCOs":[], "umbrellas":[], "countries":[], "categories":[]};
+
+//----------------------------------------MCOs QUERY ----------------------------------------------
+var query = xtk.queryDef.create(
+    <queryDef schema="nms:localOrgUnit" operation="select">
+      <select>
+        <node expr="[@label]" alias="@label" />
+        <node expr="[@abbreviation]" alias="@MCOCode" />
+      </select>
+      <orderBy displayOrder="Label (ascending)">
+         <node expr="[@label]" label="label" sort="1" sortAsc="1"/>
+        </orderBy>
+      <where>
+      <condition expr={"[@id]> 0 AND [@nature]= 'MCO'"}/>
+      </where>
+     </queryDef>
+   )
+
+var  MCOs = query.ExecuteQuery();
+
+for each (var MCO in MCOs){
+      results.MCOs.push({"marketLabel" :MCO.@label.toString(),
+      "marketCountryCode" :MCO.@MCOCode.toString()
+    });
+}
+
+//----------------------------------------Umbrellas QUERY ----------------------------------------------
+var query = xtk.queryDef.create(
+    <queryDef schema="nms:localOrgUnit" operation="select">
+      <select>
+        <node expr="[@label]" alias="@label" />
+        <node expr="[@abbreviation]" alias="@umbrellaCode" />
+      </select>
+      <orderBy displayOrder="Label (ascending)">
+         <node expr="[@label]" label="label" sort="1" sortAsc="1"/>
+        </orderBy>
+      <where>
+      <condition expr={"[@id]> 0 AND [@nature]= 'Umbrella'"}/>
+      </where>
+     </queryDef>
+   )
+
+var  umbrellas = query.ExecuteQuery();
+
+for each (var umbrella in umbrellas){
+      results.umbrellas.push({"marketLabel" :umbrella.@label.toString(),
+      "marketCountryCode" :umbrella.@umbrellaCode.toString()
+    });
+}
+
+
+//----------------------------------------COUNTRIES QUERY ----------------------------------------------
+    var query = xtk.queryDef.create(
+        <queryDef schema="nms:localOrgUnit" operation="select">
+          <select>
+            <node expr="[@label]" alias="@label" />
+            <node expr="[@abbreviation]" alias="@countryCode" />
+          </select>
+          <orderBy displayOrder="Label (ascending)">
+             <node expr="[@label]" label="label" sort="1" sortAsc="1"/>
+            </orderBy>
+          <where>
+          <condition expr={"[@id]> 0 AND [@nature]= 'Country'"}/>
+          </where>
+         </queryDef>
+       )
+
+var  countries = query.ExecuteQuery();
+
+for each (var country in countries){
+          results.countries.push({"marketLabel" :country.@label.toString(),
+          "marketCountryCode" :country.@countryCode.toString()
+        });
+}
+
+
+
+
+    document.write(JSON.stringify(results));
+    console.log(JSON.stringify(results))
+%><% logonWithContext(context);%>
--- bc_project/getOffers.js
+++ bc_project/getOffers.js
@@ -15,8 +15,28 @@
         <queryDef schema="nms:offer" operation="select">
           <select>
             <node expr="[@label]" alias="@label" />
+            <node expr="[@id]" alias="@id" />
             <node expr="[@status]" alias="@status"/>
-            <node expr="[@created]" sort="true" />
+            <node expr="[@created]" sort="true" alias="@created"/>
+            //offer content fields below
+            <node expr="[view/emailSubject1_jst]" alias="@subject1"/>
+            <node expr="[view/emailSubject2_jst]" alias="@subject2"/>
+            <node expr="[view/preHeader_jst]" alias="@preheader"/>
+            <node expr="[view/shortContent_jst]" alias="@headline"/>
+            <node expr="[view/subHeading_jst]" alias="@subheadline"/>
+            <node expr="[view/eventDate2_jst]" alias="@eventDate"/>
+            <node expr="[view/cTAText_jst]" alias="@CTAtext"/>
+            <node expr="[view/trackedUrls/url_jst]" alias="@destinationURL"/>
+            <node expr="[view/imageUrl_jst]" alias="@imageDesktopURL"/>
+            <node expr="[view/mImageUrl_jst]" alias="@imageMobileURL"/>
+            <node expr="[view/imageAltText_jst]" alias="@imageAltText"/>
+            <node expr="[view/rating_jst]" alias="@rating"/>
+            <node expr="[view/loyaltyPoints_jst]" alias="@loyaltyPoints"/>
+            <node expr="[view/unitQuantity_jst]" alias="@unitQuantity"/>
+            <node expr="[view/unitPrice_jst]" alias="@unitPrice"/>
+            <node expr="[view/uTMTerm_jst]" alias="@utmTerm"/>
+            <node expr="[view/uTMTermImage_jst]" alias="@utmImageTerm"/>
+            <node expr="[view/htmlSource_jst]" alias="@copyText"/>
           </select>
             <orderBy displayOrder="Creation date (descending)">
              <node expr="[@created]" label="Creation date" sort="1" sortDesc="1"/>
@@ -24,7 +44,7 @@
            <groupBy/>
            <having/>
           <where>
-          <condition expr={"[category/@label]= 'Recipe Recommendation' AND [localOrgUnit/@label]= 'Germany'"}/>
+          <condition expr={"[category/@label]= 'Recipe Recommendation' AND [localOrgUnit/@label]= '"+chosenMarket+"'"}/>
           </where>
          </queryDef>
        )
@@ -32,13 +52,64 @@
 var  selectedOffers = query.ExecuteQuery();
 
 for each (var offer in selectedOffers){
+    var offerBlock = {
+      "offerLabel" :offer.@label.toString(),
+      "offerId" :offer.@id.toString(),
+      "status" :offer.@status.toString(),
+      "created" :offer.@created.toString(),
+      //offer content fields below
+      "subject1" :offer.@subject1.toString(),
+      "subject2" :offer.@subject2.toString(),
+      "preheader" :offer.@preheader.toString(),
+      "headline" :offer.@headline.toString(),
+      "subheadline" :offer.@subheadline.toString(),
+      "eventDate" :offer.@eventDate.toString(),
+      "CTAtext" :offer.@CTAtext.toString(),
+      "destinationURL" :offer.@destinationURL.toString(),
+      "imageDesktopURL" :offer.@imageDesktopURL.toString(),
+      "imageMobileURL" :offer.@imageMobileURL.toString(),
+      "imageAltText" :offer.@imageAltText.toString(),
+      "rating" :offer.@rating.toString(),
+      "loyaltyPoints" :offer.@loyaltyPoints.toString(),
+      "unitQuantity" :offer.@unitQuantity.toString(),
+      "unitPrice" :offer.@unitPrice.toString(),
+      "utmTerm" :offer.@utmTerm.toString(),
+      "utmImageTerm" :offer.@utmImageTerm.toString(),
+      "copyText" :offer.@copyText.toString(),
+      "eligibility": [
+      ]
+    }
+    results.push(offerBlock);
+          
+      var offerId = offer.@id;
 
-    var block = {
-        "offerId" :offer.@id.toString(),
-        "labelonoffer" :offer.@label.toString(),
-        "category" :offer.category.@label.toString()
-          }
-          results.push(block);
+      var query2 = xtk.queryDef.create(
+        <queryDef schema="nms:offerContext" operation="select">
+          <select>
+            <node expr="[@label]" alias="@label" />
+            <node expr="[@startDate]" alias="@startDate" />
+            <node expr="[@endDate]" alias="@endDate"/>
+            <node expr="[@weightExpr]" alias="@weightExpr"/>
+          </select>
+            <groupBy/>
+            <having/>
+          <where>
+          <condition expr={"[@offer-id]= '"+offerId+"'"}/>
+          </where>
+        </queryDef>
+      )
+      
+      var eligibilities = query2.ExecuteQuery();
+      for each (var eligibility in eligibilities){
+
+        var eligibilityBlock = {
+          "label" : eligibility.@label.toString(),
+          "startDate" : eligibility.@startDate.toString(),
+          "endDate" : eligibility.@endDate.toString(),
+          "weight" : eligibility.@weightExpr.toString()
+        }
+        offerBlock.eligibility.push(eligibilityBlock);
+      }
 }
 
 
